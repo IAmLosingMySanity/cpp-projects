33c24cc4e75b5aa493381d821aec6a454822e524
justLopatin <trufanovila59@gmail.com>
2023-09-21 08:08:01 +0300

    second commit

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -6,18 +6,31 @@ using namespace std;
 int main() {
     double x;
     int n;
+    double an;
+
+    double eps = 0.001;
 
     cout << "Enter X: ";
     cin >> x;
 
-    cout << "Enter N: ";
-    cin >> n;
+    // cout << "Enter N: ";
+    // cin >> n;
 
     double sum = 0.0;
 
-    for (int i = 1; i <= n; ++i) {
-        sum += pow(x, i) / pow(i, 2);
+    int i =1;
+
+    while ((fabs(an)>eps) || sum==0)    
+    {
+        an = pow(x, i) / pow(i, 2);
+        sum += an;
+        i++;
     }
+    
+
+    // for (int i = 1; i <= n; ++i) {
+    //     sum += pow(x, i) / pow(i, 2);
+    // }
 
     cout << "Sum is " << sum << endl;
 
