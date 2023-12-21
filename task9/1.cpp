9b55af6052cb73e70a28fa5593bfc593a9388d62
justLopatin <trufanovila59@gmail.com>
2023-12-21 09:27:18 +0300

    clean

--- task9/1.cpp
+++ task9/1.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 int main()
 {
-  double x;
-  double eps = 1000000;
-  double an = 1;
-  double sum = 0;
-  double n = 0;
-  
-  std::cout << "Enter X: " << std::endl;
-  while (!(std::cin >> x) || (std::cin.peek() != '\n'))
-  {
-    std::cin.clear();
-    while (std::cin.get() != '\n');
-    std::cout << "Input error! Repeat please..." << std::endl;
-  } 
-  
-  while (std::fabs(an) < eps)
-  {
-    n += 1.0;
-    an = std::pow(x, n) / std::pow(n, 2);
-    sum += an;
-    std::cout << an << std::endl;
-  }
-  
-  std::cout << std::fixed << sum << " ";
-  
-  return 0;
+    double x;
+    constexpr double eps = 1000000;
+    double an = 1;
+    double sum = 0;
+    double n = 0;
+
+    std::cout << "Enter X: " << std::endl;
+    while (!(std::cin >> x) || (std::cin.peek() != '\n'))
+    {
+        std::cin.clear();
+        while (std::cin.get() != '\n');
+        std::cout << "Input error! Repeat please..." << std::endl;
+    }
+
+    while (std::fabs(an) < eps)
+    {
+        n += 1.0;
+        an = std::pow(x, n) / std::pow(n, 2);
+        sum += an;
+        std::cout << an << std::endl;
+    }
+
+    std::cout << std::fixed << sum << " ";
+
+    return 0;
 }
